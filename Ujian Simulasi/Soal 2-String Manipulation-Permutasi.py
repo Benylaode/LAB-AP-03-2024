0590adb4b239e28bd4a6779dd20ac5605e2a51bb
Muhammad Alif Sakti <muhammadalifsakti@gmail.com>
2024-11-28 16:08:16 +0800

    Delete Ujian Simulasi directory

--- Ujian Simulasi/Soal 2-String Manipulation-Permutasi.py
+++
@@ -1,8 +0,0 @@
-from itertools import permutations
-def unik_permutations(s):
-    perm = permutations(s)
-    unik_perm = sorted(set([''.join(p) for p in perm]))
-    return unik_perm
-Hasil = unik_permutations
-print(Hasil(input(':')))
-
--- Ujian Simulasi/Soal 3-Functional Programming-Filter dan Reduce.py 3.py
+++
@@ -1,5 +0,0 @@
-import reduce
-angka = list(map(int, input('Masukkan list angka (pisahkan dengan spasi): ').split()))
-angka_genap = list(filter(lambda x: x % 2 == 0, angka))
-jumlah_genap = reduce(lambda x, y: x + y, angka_genap)
-print('Jumlah angka genap:', jumlah_genap)

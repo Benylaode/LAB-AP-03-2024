a608e64da52ebdb4348bb578b44f5c5c51e50d86
niawwdumpiee <trivaniakaroma@gmail.com>
2024-11-29 21:10:13 +0700

    no.4

--- H071241039/UJIAN TAMBAHAN/UT-NO-4.py
+++ H071241039/UJIAN TAMBAHAN/UT-NO-4.py
@@ -1,16 +1,18 @@
 usia = input("masukkan usia: ")
-
-usia = int(usia)
-if usia < 0:
-    print("usia tidak boleh negatif atau bukan angka")
-else:
-    if usia < 12:
-        kategori = "anak-anak"
-    elif 12 <= usia <= 17:
-        kategori = "remaja"
-    elif 18 <= usia <= 64:
-        kategori = "dewasa"
+try:
+    usia = int(usia)
+    if usia < 0:
+        print("usia tidak boleh negatif atau bukan angka")
     else:
-        kategori = "lansia"
+        if usia < 12:
+            kategori = "anak-anak"
+        elif 12 <= usia <= 17:
+            kategori = "remaja"
+        elif 18 <= usia <= 64:
+            kategori = "dewasa"
+        else:
+            kategori = "lansia"
         
-print(f"anda termasuk dalam kategori {kategori}")
+        print(f"anda termasuk dalam kategori {kategori}")
+except:
+    print("input tidak valid.")
